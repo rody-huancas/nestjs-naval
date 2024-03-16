07732ab1273ce0b60d6299939a2544201bcd2a70
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-15 20:41:35 -0500

    Add: entity incident

--- src/module/incidents/dto/create-incident.dto.ts
+++ src/module/incidents/dto/create-incident.dto.ts
@@ -1,1 +1,22 @@
-export class CreateIncidentDto {}
+import { IsString, IsUUID, IsNotEmpty } from 'class-validator';
+
+export class CreateIncidentDto {
+  @IsString()
+  @IsNotEmpty()
+  in_description: string;
+
+  @IsUUID()
+  in_iduser: string;
+
+  @IsUUID()
+  in_idtypein: string;
+
+  @IsString()
+  in_date: string;
+
+  @IsString()
+  in_site: string;
+
+  @IsUUID()
+  in_idcomp: string;
+}
--- src/module/incidents/entities/incident.entity.ts
+++ src/module/incidents/entities/incident.entity.ts
@@ -1,1 +1,32 @@
-export class Incident {}
+import { Company } from 'src/module/companies/entities/company.entity';
+import { TypeIncident } from 'src/module/type-incidents/entities/type-incident.entity';
+import { User } from 'src/module/users/entities/user.entity';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+@Entity({ name: 'incident' })
+export class Incident {
+  @PrimaryGeneratedColumn('uuid')
+  in_id: string;
+
+  @Column({ type: 'varchar', unique: true })
+  in_description: string;
+
+  
+  @Column({ type: 'string', default: true })
+  in_date: string;
+  
+  @Column({ type: 'string', default: true })
+  in_site: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'in_iduser' })
+  user: User;
+
+  @ManyToOne(() => TypeIncident)
+  @JoinColumn({ name: 'in_idtypein' })
+  typeIncident: TypeIncident;
+
+  @ManyToOne(() => Company)
+  @JoinColumn({ name: 'in_idcomp' })
+  company: Company;
+}
